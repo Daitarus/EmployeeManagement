ee79ea0a863f98ddbaac8bfabcfa1d1c0478d120
PavelChaikovskii <chajkovskij_99@mail.ru>
2024-07-23 20:10:30 +0300

    feat: add service interfaces

---
+++ EmployeeManagementCLI/Services/Interfaces/IRecorderService.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagementCLI.Services.Interfaces
+{
+    public interface IRecorderService
+    {
+        public T? ReadModel<T>() where T : class;
+
+        public void WriteModel<T>(T model) where T : class;
+
+        public Task<T?> ReadModelAsync<T>() where T : class;
+
+        public Task WriteModelAsync<T>(T model) where T : class;
+    }
+}
---
+++ EmployeeManagementCLI/Services/Interfaces/IRepositoryService.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagementCLI.Entities.Base;
+
+namespace EmployeeManagementCLI.Services.Interfaces
+{
+    public interface IRepositoryService<T> where T : Entity
+    {
+        public void AddEntity(T entity);
+
+        public void UpdateEntity(T entity);
+
+        public T GetEntity(int id);
+
+        public void DeleteEntity(int id);
+    }
+}
