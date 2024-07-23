0d7e2944b3e1b2d3e7daa2a49ca5025ce8e7dcaf
Pavel Chaikovskii <chajkovskij_99@mail.ru>
2024-07-23 17:29:18 +0300

    feat: add JsonService

---
+++ EmployeeManagementCLI/Services/JsonService.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeManagementCLI.Models;
+using System.Text.Json;
+
+namespace EmployeeManagementCLI.Services
+{
+    public class JsonService
+    {
+        public string JsonPath { private get; set; }
+
+        public JsonService(string jsonPath)
+        {
+            JsonPath = jsonPath;
+        }
+
+        public async Task<T?> ReadModel<T>() where T : class
+        {
+            using (FileStream fs = new FileStream(JsonPath, FileMode.OpenOrCreate))
+            {
+                return await JsonSerializer.DeserializeAsync<T>(fs);
+            }
+        }
+
+        public async Task WriteModel<T>(T model) where T : class
+        {
+            using (FileStream fs = new FileStream(JsonPath, FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync(fs, model);
+            }
+        }
+    }
+}
