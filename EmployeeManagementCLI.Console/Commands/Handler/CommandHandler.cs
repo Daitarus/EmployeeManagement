2713ed4ffb89e237874f2b2d107e9ea9d9f8cbac
PavelChaikovskii <chajkovskij_99@mail.ru>
2024-07-24 17:59:35 +0300

    feat: add CommandHandler

---
+++ EmployeeManagementCLI.Console/Commands/Handler/CommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagementCLI.Console.Commands.Controllers.Interfaces;
+using EmployeeManagementCLI.Console.Commands.Handler.Interfaces;
+using EmployeeManagementCLI.Console.Commands.Models;
+
+namespace EmployeeManagementCLI.Console.Commands.Handler
+{
+    public class CommandHandler : ICommandHandler
+    {
+        private ICommandController _controller;
+
+        public CommandHandler(ICommandController controller)
+        {
+            _controller = controller;
+        }
+
+        public string Execute(Command command)
+        {
+            return command.Type switch
+            {
+                CommandType.Add => _controller.Add(command),
+                CommandType.Update => _controller.Update(command),
+                CommandType.Delete => _controller.Delete(command),
+                CommandType.Get => _controller.Get(command),
+                CommandType.GetAll => _controller.GetAll(command),
+                _ => _controller.Unknown(command)
+            };
+        }
+    }
+}
