67c6bc3f702f916240cd4583ec013d08dfc2e6d7
PavelChaikovskii <chajkovskij_99@mail.ru>
2024-07-25 23:56:09 +0300

    feat: complete EmployeeService

--- EmployeeManagementCLI.Domain/Models/Message.cs
+++ EmployeeManagementCLI.Domain/Models/Message.cs
@@ -2,11 +2,20 @@
 {
     public class Message
     {
+        public ActionStatus Status { get; set; }
         public string Text { get; set; }
 
-        public Message(string text)
+        public Message(ActionStatus status, string text)
         {
+            Status = status;
             Text = text;
         }
+    }
+
+    public enum ActionStatus
+    {
+        Error,
+        Success,
+        NotSuccess
     }
 }
--- EmployeeManagementCLI.Domain/Services/EmployeeService.cs
+++ EmployeeManagementCLI.Domain/Services/EmployeeService.cs
@@ -2,6 +2,7 @@
 using EmployeeManagementCLI.Domain.Models;
 using EmployeeManagementCLI.Domain.Services.Interfaces;
 using Microsoft.Extensions.Logging;
+using System.Text;
 
 namespace EmployeeManagementCLI.Domain.Services
 {
@@ -16,29 +17,176 @@ namespace EmployeeManagementCLI.Domain.Services
             _logger = logger;
         }
 
+        #region Service action
         public Message AddEmployee(Employee employee)
         {
-            
+            if (employee == null) throw new ArgumentNullException(nameof(employee));
+
+            if(string.IsNullOrEmpty(employee.FirstName) || string.IsNullOrEmpty(employee.LastName) || employee.SalaryPerHous == null)
+            {
+                _logger?.LogWarning($"[{nameof(AddEmployee)}]: Warning: not all data is filled for action!");
+                return new Message(ActionStatus.NotSuccess, $"Warning: not all data is filled for action");
+            }
+
+            try
+            {
+                var employeeEntity = new Data.Entities.Employee()
+                {
+                    FirstName = employee.FirstName,
+                    LastName = employee.LastName,
+                    SalaryPerHous = (decimal)employee.SalaryPerHous 
+                };
+
+                employeeEntity = _context.AddEntity(employeeEntity);
+                _context.SaveChanges();
+
+                _logger?.LogInformation($"[{nameof(AddEmployee)}][Id = {employee.Id}]: Success!");
+                return new Message(ActionStatus.Success, $"Employee was added (Id = {employee.Id})");
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError($"[{nameof(AddEmployee)}]: {ex.Message}", ex);
+                return new Message(ActionStatus.Error, "Error: action failed!");
+            }
         }
 
         public Message DeleteEmployee(int id)
         {
-            
+            try
+            {
+                _context.DeleteEntity(id);
+                var changeCount = _context.SaveChanges();
+
+                if (changeCount == 0)
+                {
+                    _logger?.LogWarning($"[{nameof(DeleteEmployee)}][Id = {id}]: Warning: no data available!");
+                    return new Message(ActionStatus.NotSuccess, $"Warning: no data available");
+                }
+                else
+                {
+                    _logger?.LogInformation($"[{nameof(DeleteEmployee)}][Id = {id}]: Success!");
+                    return new Message(ActionStatus.Success, $"Employee with Id = {id} was deleted!");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError($"[{nameof(DeleteEmployee)}][Id = {id}]: {ex.Message}", ex);
+                return new Message(ActionStatus.Error, "Error: action failed!");
+            }
         }
 
         public Message GetAllEmployees()
         {
-            
+            try
+            {
+                var employees = _context.GetAllEntities();
+
+                if (employees == null || employees?.Count() == 0)
+                {
+                    _logger?.LogWarning($"[{nameof(GetAllEmployees)}]: Warning: no data available!");
+                    return new Message(ActionStatus.NotSuccess, "Warning: no data available!");
+                }
+                else
+                {
+                    var resultText = new StringBuilder();
+                    foreach (var employee in employees)
+                    {
+                        resultText.AppendLine(CreateEmployeeInfoStr(employee));
+                    }
+
+                    _logger?.LogInformation($"[{nameof(GetAllEmployees)}]: Success!");
+                    return new Message(ActionStatus.Success, resultText.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError($"[{nameof(GetAllEmployees)}]: {ex.Message}", ex);
+                return new Message(ActionStatus.Error, "Error: action failed!");
+            }
         }
 
         public Message GetEmployee(int id)
         {
-            
+            try
+            {
+                var employee = _context.GetEntity(id);
+
+                if (employee == null)
+                {
+                    _logger?.LogWarning($"[{nameof(GetEmployee)}][Id = {id}]: Warning: no data available!");
+                    return new Message(ActionStatus.NotSuccess, "Warning: no data available!");
+                }
+                else
+                {
+                    _logger?.LogInformation($"[{nameof(GetEmployee)}][Id = {id}]: Success!");
+                    return new Message(ActionStatus.Success, CreateEmployeeInfoStr(employee));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError($"[{nameof(GetEmployee)}][Id = {id}]: {ex.Message}", ex);
+                return new Message(ActionStatus.Error, "Error: action failed!");
+            }
         }
 
         public Message UpdateEmployee(Employee employee)
         {
-            
+            if (employee == null) throw new ArgumentNullException(nameof(employee));
+
+            if (string.IsNullOrEmpty(employee.FirstName) && string.IsNullOrEmpty(employee.LastName) && employee.SalaryPerHous == null)
+            {
+                _logger?.LogWarning($"[{nameof(UpdateEmployee)}][Id = {employee.Id}]: Warning: no updated data!");
+                return new Message(ActionStatus.NotSuccess, $"Warning: no updated data");
+            }
+
+            try
+            {
+                var employeeEntity = new Data.Entities.Employee()
+                {
+                    FirstName = employee.FirstName,
+                    LastName = employee.LastName,
+                    SalaryPerHous = (decimal)employee.SalaryPerHous
+                };
+
+                employeeEntity = _context.AddEntity(employeeEntity);
+                var changeCount = _context.SaveChanges();
+
+                if (changeCount == 0)
+                {
+                    _logger?.LogWarning($"[{nameof(UpdateEmployee)}][Id = {employeeEntity.Id}]: Warning: no data available!");
+                    return new Message(ActionStatus.NotSuccess, $"Warning: no data available");
+                }
+                else
+                {
+                    _logger?.LogInformation($"[{nameof(UpdateEmployee)}][Id = {employeeEntity.Id}]: Success!");
+                    return new Message(ActionStatus.Success, $"Employee with Id = {employeeEntity.Id} was updated!");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError($"[{nameof(UpdateEmployee)}][Id = {employee.Id}]: {ex.Message}", ex);
+                return new Message(ActionStatus.Error, "Error: action failed!");
+            }
+        }
+        #endregion
+
+        private string CreateEmployeeInfoStr(Data.Entities.Employee employee)
+        {
+            var info = new StringBuilder();
+
+            info.Append("Id = ");
+            info.Append(employee.Id);
+
+            info.Append(", FirstName = ");
+            info.Append(employee.FirstName);
+
+            info.Append(", LastName = ");
+            info.Append(employee.LastName);
+
+            info.Append(", SalaryPerHour = ");
+            info.Append(employee.SalaryPerHous);
+
+            return info.ToString();
         }
     }
 }
