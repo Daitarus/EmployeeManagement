0b035517a0ad1bfb6d21d3a72727bf049ff4beb6
PavelChaikovskii <chajkovskij_99@mail.ru>
2024-07-25 17:46:21 +0300

    feat: controller complete

--- EmployeeManagementCLI.Console/Commands/Controllers/CommandController.cs
+++ EmployeeManagementCLI.Console/Commands/Controllers/CommandController.cs
@@ -1,43 +1,85 @@
 ﻿using EmployeeManagementCLI.Console.Commands.Controllers.Interfaces;
+using EmployeeManagementCLI.Console.Commands.Handler.Interfaces;
 using EmployeeManagementCLI.Console.Commands.Models;
+using EmployeeManagementCLI.Domain.Models;
+using EmployeeManagementCLI.Domain.Services.Interfaces;
 
 namespace EmployeeManagementCLI.Console.Commands.Controllers
 {
     public class CommandController : ICommandController
     {
-        public CommandController()
+        private readonly IEmployeeService _employeeService;
+        private readonly IModelConverter<Command, Employee> _employeeConverter;
+        private readonly IModelConverter<Command, int> _idConverter;
+        private readonly IModelConverter<Message, string> _messageConverter;
+
+        public CommandController(IEmployeeService employeeService, 
+            IModelConverter<Command, Employee> employeeConverter, 
+            IModelConverter<Command, int> idConverter, 
+            IModelConverter<Message, string> messageConverter)
         {
-            
+            _employeeService = employeeService;
+            _employeeConverter = employeeConverter;
+            _idConverter = idConverter;
+            _messageConverter = messageConverter;
         }
 
         public string Add(Command command)
         {
-            throw new NotImplementedException();
+            var validCommandType = CommandType.Add;
+            if (command.Type != validCommandType) throw new ArgumentException($"Command Type must be {validCommandType}", nameof(command));
+
+            var employee = _employeeConverter.Convert(command);
+            var message = _employeeService.AddEmployee(employee);
+            return _messageConverter.Convert(message);
         }
 
         public string Delete(Command command)
         {
-            throw new NotImplementedException();
+            var validCommandType = CommandType.Delete;
+            if (command.Type != validCommandType) throw new ArgumentException($"Command Type must be {validCommandType}", nameof(command));
+
+            var id = _idConverter.Convert(command);
+            var message = _employeeService.DeleteEmployee(id);
+            return _messageConverter.Convert(message);
         }
 
         public string Get(Command command)
         {
-            throw new NotImplementedException();
+            var validCommandType = CommandType.Get;
+            if (command.Type != validCommandType) throw new ArgumentException($"Command Type must be {validCommandType}", nameof(command));
+
+            var id = _idConverter.Convert(command);
+            var message = _employeeService.GetEmployee(id);
+            return _messageConverter.Convert(message);
         }
 
         public string GetAll(Command command)
         {
-            throw new NotImplementedException();
+            var validCommandType = CommandType.GetAll;
+            if (command.Type != validCommandType) throw new ArgumentException($"Command Type must be {validCommandType}", nameof(command));
+
+            var message = _employeeService.GetAllEmployees();
+            return _messageConverter.Convert(message);
         }
 
         public string Unknown(Command command)
         {
-            throw new NotImplementedException();
+            var validCommandType = CommandType.Unknown;
+            if (command.Type != validCommandType) throw new ArgumentException($"Command Type must be {validCommandType}", nameof(command));
+
+            //TODO add UnknownCommandService
+            return "There is no such command!";
         }
 
         public string Update(Command command)
         {
-            throw new NotImplementedException();
+            var validCommandType = CommandType.Update;
+            if (command.Type != validCommandType) throw new ArgumentException($"Command Type must be {validCommandType}", nameof(command));
+
+            var employee = _employeeConverter.Convert(command);
+            var message = _employeeService.UpdateEmployee(employee);
+            return _messageConverter.Convert(message);
         }
     }
 }
